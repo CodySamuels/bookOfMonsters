cd3cc75a0b959f71ea9f2ffb26d254d6d621f82d
Cody Samuels <twowoodenspoons@gmail.com>
2022-04-04 22:29:33 -0700

    parepre to deploy

--- db/migrate/20220405031534_create_monsters.rb
+++ db/migrate/20220405031534_create_monsters.rb
@@ -1,9 +1,9 @@
 class CreateMonsters < ActiveRecord::Migration[7.0]
   def change
     create_table :monsters do |t|
-      t.string :name
-      t.string :ac
-      t.string :hp
+      t.text :name
+      t.text :ac
+      t.text :hp
 
       t.timestamps
     end
